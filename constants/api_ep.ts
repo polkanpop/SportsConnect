6dcbdfd6f44e3807234b7c73e8a83f7d0fd90fee
LeDucTri1484 <trildsws00497@fpt.edu.vn>
2025-11-07 21:50:24 +0700

    m

--- constants/api_ep.ts
+++
@@ -1,4 +0,0 @@
-export const API_ENDPOINTS = {
-  USERS: '/users',
-  EVENTS: '/events',
-};
--- metro.config.js
+++
@@ -1,6 +0,0 @@
-// Re-enable NativeWind via Metro wrapper instead of Babel plugin to avoid
-// triggering the mysterious Babel '.plugins' option error.
-const { getDefaultConfig } = require('expo/metro-config')
-const { withNativeWind } = require('nativewind/metro')
-const config = getDefaultConfig(__dirname)
-module.exports = withNativeWind(config, { input: './app/global.css' })
