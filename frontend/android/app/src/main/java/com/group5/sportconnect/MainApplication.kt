edfc67b4b6d6e4f1dd2b701444c9a9ff6a2ec6f5
LeDucTri1484 <trildsws00497@fpt.edu.vn>
2026-04-03 19:35:36 +0700

    native: Zalo SDK init + meta-data + appID + adaptive icon smaller ball

--- frontend/android/app/src/main/java/com/group5/sportconnect/MainApplication.kt
+++ frontend/android/app/src/main/java/com/group5/sportconnect/MainApplication.kt
@@ -15,6 +15,7 @@ import com.facebook.react.defaults.DefaultReactNativeHost
 
 import expo.modules.ApplicationLifecycleDispatcher
 import expo.modules.ReactNativeHostWrapper
+import com.zing.zalo.zalosdk.oauth.ZaloSDK
 
 class MainApplication : Application(), ReactApplication {
 
@@ -47,6 +48,7 @@ class MainApplication : Application(), ReactApplication {
     }
     loadReactNative(this)
     ApplicationLifecycleDispatcher.onApplicationCreate(this)
+    ZaloSDK.Instance.init(this)
   }
 
   override fun onConfigurationChanged(newConfig: Configuration) {
